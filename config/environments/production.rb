d61021134960ade2d0534e235bc01c677a6e50a4
Chelsi Jones <cajones.bz@gmail.com>
2018-02-24 09:07:03 -0600

    Corrected SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,8 +68,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'warm-ocean-61529.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.stmp_settings = {
-    :address              => 'stmp.sendgrid.net',
+  ActionMailer::Base.smtp_settings = {
+    :address              => 'smtp.sendgrid.net',
     :port                 => '587',
     :authentication       => :plain,
     :user_name            => ENV['SENDGRID_USERNAME'],
